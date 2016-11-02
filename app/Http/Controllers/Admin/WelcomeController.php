44bbb41405f24f892ff38d90d4403d46afb7798c
Chayan Al Amin <chayan.php@gmail.com>
2016-11-02 23:37:41 +0600

    find popular tags

--- app/Http/Controllers/Admin/WelcomeController.php
+++ app/Http/Controllers/Admin/WelcomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Admin\AdminController AS Controller;
+use App\Models\Tag;
 
 class WelcomeController extends Controller
 {
@@ -13,6 +14,13 @@ class WelcomeController extends Controller
      */
     public function index()
     {
+        $tags = Tag::whereHas('views', function ($query) {
+                    $query->orderBy('view_count', 'desc');
+                })->with(['views' => function($query) {
+                    $query->select('view_count');
+                }])->select(['id', 'name', 'slug'])
+                        ->take(5)
+                        ->get();
         return view('admin.welcome.index');
     }
 }
